42280413f6051e6acaa3aee9c7812944bca6b59f
David Piper <dbpiper@gmail.com>
2018-09-26 11:55:54 -0500

    Add Kata project.

---
+++ Kata/Kata.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Kata {
+    partial class Kata {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonRandomSelect = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.comboBoxLesson = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 49);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Randomly Select";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // buttonRandomSelect
+            // 
+            this.buttonRandomSelect.Location = new System.Drawing.Point(75, 115);
+            this.buttonRandomSelect.Name = "buttonRandomSelect";
+            this.buttonRandomSelect.Size = new System.Drawing.Size(75, 49);
+            this.buttonRandomSelect.TabIndex = 0;
+            this.buttonRandomSelect.Text = "Randomly Select";
+            this.buttonRandomSelect.UseVisualStyleBackColor = true;
+            this.buttonRandomSelect.Click += new System.EventHandler(this.buttonRandomSelect_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(72, 203);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(47, 13);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "Exercise";
+            // 
+            // comboBoxLesson
+            // 
+            this.comboBoxLesson.FormattingEnabled = true;
+            this.comboBoxLesson.Location = new System.Drawing.Point(75, 75);
+            this.comboBoxLesson.Name = "comboBoxLesson";
+            this.comboBoxLesson.Size = new System.Drawing.Size(387, 21);
+            this.comboBoxLesson.TabIndex = 3;
+            this.comboBoxLesson.SelectedIndexChanged += new System.EventHandler(this.comboBoxLesson_SelectedIndexChanged);
+            // 
+            // Kata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 450);
+            this.Controls.Add(this.comboBoxLesson);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonRandomSelect);
+            this.Controls.Add(this.button3);
+            this.Name = "Kata";
+            this.Text = "Kata";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonRandomSelect;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.ComboBox comboBoxLesson;
+    }
+}
+
---
+++ Kata/Kata.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kata {
+    public partial class Kata : Form {
+        public Kata() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void PopulateList(string filePath) {
+            dynamic katas = LoadJson(filePath);
+            JArray array = JArray.Parse(katas.Lessons.List.ToString());
+            comboBoxLesson.Items.AddRange(array.ToObject<List<String>>().ToArray());
+        }
+
+        public dynamic LoadJson(string file) {
+            using (StreamReader r = new StreamReader(file)) {
+                string json = r.ReadToEnd();
+                dynamic jsonObject = JObject.Parse(json);
+                return jsonObject;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            PopulateList("Configuration\\JSON\\DrawaboxKatas.json");
+        }
+
+        private void comboBoxLesson_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+           
+        private int RandomNumber(int max)
+        {
+            Random rng = new Random();
+            return rng.Next(0, max);
+        }
+
+        private int SelectLessonNum(dynamic katas) {
+            int numLessons = katas.Lessons.Content.Count;
+            return RandomNumber(numLessons - 1);
+        }
+
+        private int SelectExerciseNum(dynamic lesson) {
+            int numExercises = lesson.Exercises.Count;
+            int exerciseNum = RandomNumber(numExercises - 1);
+            return exerciseNum;
+        }
+
+        private dynamic SelectEverydayObject(int exerciseNum) {
+            string everydayObjectsFile = "Configuration\\JSON\\EverydayObjects.json";
+            dynamic everydayObjects = LoadJson(everydayObjectsFile);
+
+            dynamic everydayObjectGroup = everydayObjects["Everyday Objects"][exerciseNum];
+            int exerciseCount = everydayObjectGroup.Content.Count;
+
+            int subExerciseNum = RandomNumber(exerciseCount - 1);
+            return everydayObjectGroup.Content[subExerciseNum]; 
+        }
+
+        private void buttonRandomSelect_Click(object sender, EventArgs e) {
+            string kataFile = "Configuration\\JSON\\DrawaboxKatas.json";
+            dynamic katas = LoadJson(kataFile);
+
+            int lessonNum = SelectLessonNum(katas);
+            dynamic lesson = null;
+
+            if (comboBoxLesson.SelectedIndex >= 0) {
+                lessonNum = comboBoxLesson.SelectedIndex;        
+            }
+
+            lesson = katas.Lessons.Content[lessonNum]; 
+
+            int exerciseNum = SelectExerciseNum(lesson); 
+            dynamic exercise = lesson.Exercises[exerciseNum];
+            dynamic subExercise = null;
+            
+            if (lessonNum == 5) {  // everyday object
+                subExercise = SelectEverydayObject(exerciseNum);  
+            }
+
+            string messageExercise = string.Format("Lesson: {0}\nExercise: {1}", lesson.Name, exercise);
+            string messageSubexercise = "";
+
+            if (subExercise != null) {
+                messageSubexercise = string.Format("\nSubexercise: {0}", subExercise);
+            }
+
+            string message = messageExercise + messageSubexercise;
+
+            labelResult.Text = message;
+        }
+    }
+}
